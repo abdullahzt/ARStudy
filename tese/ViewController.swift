556635b36afe75bf11e4f58df4c1712fb924da9a
Abdullah Zubair Tanwir <abdullahzubair075@gmail.com>
2020-04-23 14:46:53 +0500

    Initial Commit

---
+++ tese/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tese
+//
+//  Created by Abdullah on 23/04/2020.
+//  Copyright © 2020 Abdullah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
